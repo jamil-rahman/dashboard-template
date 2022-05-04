5cda0e8e647b9121fd58005998d565ff4260ae2c
Jamilur Rahman <r.jamilur96@gmail.com>
2022-05-04 18:10:00 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      background: "#F2F2F3",
+      primary: "#00C614",
+      neutral: {
+        100: "#6B6C72",
+        800: "#88898E",
+        900: "#3E3F48",
+      },
+      warning: {
+        100: "#FDF4ED",
+        200: "#F58A28",
+      },
+      success: "#44C776",
+      error: "#F52828",
+    },
+    fontFamily: {
+      body: ["Inter"]
+    },
+    extend: {},
+  },
+  plugins: [],
+};
